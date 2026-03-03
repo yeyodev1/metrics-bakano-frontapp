1e3a90959aac811b28df61c3cedb79c98561a0dc
Diego Reyes <diegorele13@gmail.com>
2026-03-03 18:49:27 -0500

    add: cosas

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    allowedHosts: ["testing-storybrand-frontend.bakano.ec"],
+  },
   build: {
     target: 'esnext',
   },
